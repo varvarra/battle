8be15325aae988b846f5aee78c63a36e6449ffd5
Samir Gossain <samir.gossain@gmail.com>
2017-01-20 15:06:06 +0000

    Stage 26: Failing Tests; Check 'Views'

--- app.rb
+++ app.rb
@@ -9,25 +9,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @game = Game.new
-    @game.attack($player_2)
-    @player_1_hp = $player_1.hit_points
-    @player_2_hp = $player_2.hit_points
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,12 +4,12 @@
     scenario 'Player 1 attacks Player 2 and receives confirmation ' do
       sign_in_and_play
       click_link("Attack!")
-      expect(page).to have_content "#{ $player_1.name} attacked #{ $player_2.name }."
+      expect(page).to have_content "#{ $game.player_1} attacked #{ $game.player_2 }."
     end
 
     scenario 'Reduce Player 2 HP by 10' do
       sign_in_and_play
       click_link("Attack!")
-      expect(page).to have_content("Its score is #{$player_2.hit_points} hit points.")
+      expect(page).to have_content("Its score is #{$game.player_2.hit_points} hit points.")
     end
   end
