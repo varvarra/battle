c4fad8847f8ee3fd4b908f094e6d15d7ccf601bd
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-01-20 18:04:30 +0000

    26 -Skinny controllers - finished

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require './lib/player'
+require './lib/player'
+
 
 
 class Battle < Sinatra::Base
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,15 +1,15 @@
-
+require "spec_helper"
   feature 'Attack' do
 
     scenario 'Player 1 attacks Player 2 and receives confirmation ' do
       sign_in_and_play
       click_link("Attack!")
-      expect(page).to have_content "#{ $game.player_1} attacked #{ $game.player_2 }."
+      expect(page).to have_content "#{ $game.player_1.name} attacked #{ $game.player_2.name }."
     end
 
     scenario 'Reduce Player 2 HP by 10' do
       sign_in_and_play
       click_link("Attack!")
-      expect(page).to have_content("Its score is #{$game.player_2.hit_points} hit points.")
+      expect(page).to have_content "Its score is #{$game.player_2.hit_points} hit points."
     end
   end
